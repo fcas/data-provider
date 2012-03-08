6798490b74f8522ded2f7ab4d7b57b9324f93304
f3l1nux@gmail.com <f3l1nux@gmail.com@db9acaa9-5aba-9e73-0ebe-e0f3ac8a038a>
2012-03-08 05:35:15 +0000

    ... depois continuo

--- data-provider/src/data/provider/DataProvider.java
+++ data-provider/src/data/provider/DataProvider.java
@@ -2,6 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package data.provider;
 import java.io.IOException;
 import java.net.DatagramPacket; 
@@ -11,11 +12,49 @@ import java.net.SocketException;
 import java.awt.BorderLayout; 
 import java.awt.event.ActionEvent; 
 import java.awt.event.ActionListener; 
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 /**
  *
  * @author felipe
  */
+
+public class Provider extends Jframe {
+
+    private JTextField enterField; // mandar discografia
+    private JTextArea displayArea; // exibir mensagem da discografia enviada
+    private DatagramSocket socket; // socket (TCP/IP) para conectar ao Servidor
+ 
+    public Provider (){
+        
+      super ("Provider");
+        
+        enterField = new JTextField ("Informe a discografia");
+        enterField.addActionListener(
+            new ActionListener ()
+            {
+                public void actionPerfomed (ActionEvent event){
+                    {
+                        try { // cria e envia o pacote  
+                            //obtem a mensagem no campos de texto
+                            String message = event.getActionCommand();
+                            displayArea.append("\nEnviando discografia:" + message + "\n");
+                            
+                            byte data[] = message.getBytes(); // converte a mensagem em bytes
+                            
+                            //cria sendPacket
+                            DatagramPacket sendPacket = new DatagramPacket (data,data.length, InetAddress.getLocalHost(), 5000);
+                    }
+        
+    }
+    }
+    
+} 
+
 public class DataProvider {
 
     /**
